060dec528dcd376f12d4651a2aa4f264a3d7ced9
AhmedHamza667 <hamzaa667788@gmail.com>
2023-06-09 12:09:13 +0300

    01

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,19 @@
 function titleCaseEdit(title) {
   // Insert code here;
+    var words = title.split(" ");
+
+    // Capitalize
+    for (var i = 0; i < words.length; i++) {
+        var word = words[i];
+        var firstLetter = word.charAt(0).toUpperCase();
+        var restOfWord = word.slice(1).toLowerCase();
+        words[i] = firstLetter + restOfWord;
+    }
+
+    // Join the words into a string
+    var revisedTitle = words.join(" ");
+
+    return revisedTitle;
 }
 
 // Do not edit this line;
