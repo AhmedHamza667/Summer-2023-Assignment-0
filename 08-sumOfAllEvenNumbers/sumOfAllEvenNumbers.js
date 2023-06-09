b96049578afaec9c10e6529ccf6bf89cf34d7536
AhmedHamza667 <hamzaa667788@gmail.com>
2023-06-09 12:49:31 +0300

    08

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -3,7 +3,7 @@ function sumOfAllEvenNumbers(nums) {
     var sum = 0;
     for (var i = 0; i < nums.length; i++) {
         if (nums[i] % 2 == 0) {
-            sum += nums[i];
+            sum++;
         }
     }
     return sum;
