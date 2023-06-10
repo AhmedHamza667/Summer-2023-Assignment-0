ed0745569f987bd97b196eeff381e9e6957c7648
AhmedHamza667 <hamzaa667788@gmail.com>
2023-06-10 04:13:22 +0300

    11

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,10 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+    var minValue = Math.min(...nums);
+    var maxValue = Math.max(...nums);
+
+    return minValue + maxValue;
+
 }
 
 // Do not edit this line;
