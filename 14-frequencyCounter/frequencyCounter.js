c14b280a9a1f252c57ce26b23eff2ff538888aad
AhmedHamza667 <hamzaa667788@gmail.com>
2023-06-10 04:20:31 +0300

    14

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,17 @@
 function frequencyCounter(word) {
   // Insert code here;
+    var frequency = {};
+
+    for (var i = 0; i < word.length; i++) {
+        var letter = word[i];
+        if (frequency[letter]) {
+            frequency[letter]++;
+        } else {
+            frequency[letter] = 1;
+        }
+    }
+
+    return frequency;
 }
 
 // Do not edit this line;
