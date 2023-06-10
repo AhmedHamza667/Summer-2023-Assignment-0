97672301019be10baf7d9a8ad3c264f53f401e2f
AhmedHamza667 <hamzaa667788@gmail.com>
2023-06-10 04:30:26 +0300

    18

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,16 @@
 function pairSum(nums, target) {
   // Insert code here;
+    const complementSet = new Set();
+
+    for (const num of nums) {
+        const complement = target - num;
+        if (complementSet.has(num)) {
+            return true; // Found a pair that adds up to the target
+        }
+        complementSet.add(complement);
+    }
+
+    return false;
 }
 
 // Do not edit this line;
