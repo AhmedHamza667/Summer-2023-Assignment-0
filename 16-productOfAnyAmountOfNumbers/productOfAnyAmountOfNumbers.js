0292b9b78b6e45cf12b7f07140a9a50cacec07bd
AhmedHamza667 <hamzaa667788@gmail.com>
2023-06-10 04:25:30 +0300

    16

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,11 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+    if (args.length === 0) {
+        return 0; // Edge case: no arguments provided, return 0
+    }
+
+    return args.reduce((accumulator, currentValue) => accumulator * currentValue);
+
 }
 
 // Do not edit this line;
