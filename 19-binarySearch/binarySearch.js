9b4503ecae6c054796b40cf0e582b315ed35d4d2
AhmedHamza667 <hamzaa667788@gmail.com>
2023-06-10 04:33:26 +0300

    19

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,27 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+        this.binarySearchRecursive(nums, target, 0, nums.length - 1);
+        return this.flag;
+    }
+
+    binarySearchRecursive(nums, target, left, right) {
+        if (left > right) {
+            return;
+        }
+
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            this.flag = true;
+            return;
+        } else if (nums[mid] > target) {
+            this.binarySearchRecursive(nums, target, left, mid - 1);
+        } else {
+            this.binarySearchRecursive(nums, target, mid + 1, right);
+        }
+    }
+}
   }
 }
 
